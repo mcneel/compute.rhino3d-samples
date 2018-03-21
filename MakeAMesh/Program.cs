ffe3487e49bcc24c62c1a470029ad0a4b3e441e3
Steve Baer <steve@mcneel.com>
2018-03-20 17:29:15 -0700

    create basic MakeAMesh sample

---
+++ MakeAMesh/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Rhino.Compute;
+
+namespace MakeAMesh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputeServer.ApiToken = enter_your_email_address;
+            var sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 12);
+            var sphereAsBrep = sphere.ToBrep();
+            // above uses standard Rhino3dmIO methods locally
+            // the following function calls compute.rhino3d.com to get access to something not
+            // available in Rhino3dmIO
+            var meshes = MeshCompute.CreateFromBrep(sphereAsBrep);
+
+            // back to regular Rhino3dmIO local calls
+            Console.WriteLine($"Got {meshes.Length} meshes");
+            for (int i = 0; i < meshes.Length; i++)
+            {
+                Console.WriteLine($"  {i + 1} mesh has {meshes[i].Vertices.Count} vrtices");
+            }
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
