326f306c68123b4b1f302d5a6c9d319e7ec879a2
Dale Fugier <dale@mcneel.com>
2018-10-15 14:27:05 -0700

    Updated NuGet package for Rhino3dmIo, updated RhinoCompute.cs to one that supports AuthToken

--- MakeAMesh/Program.cs
+++ MakeAMesh/Program.cs
@@ -7,22 +7,25 @@ namespace MakeAMesh
     {
         static void Main(string[] args)
         {
-            ComputeServer.ApiToken = "steve@mcneel.com";
+            // TODO: Add authorization token here
+            ComputeServer.AuthToken = "<Add authorization token here>";
+
+            // Use standard Rhino3dmIO methods locally
             var sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 12);
             var sphereAsBrep = sphere.ToBrep();
-            // above uses standard Rhino3dmIO methods locally
-            // the following function calls compute.rhino3d.com to get access to something not
+
+            // The following function calls compute.rhino3d.com to get access to something not
             // available in Rhino3dmIO
             var meshes = MeshCompute.CreateFromBrep(sphereAsBrep);
 
-            // back to regular Rhino3dmIO local calls
+            // Back to regular Rhino3dmIO local calls
             Console.WriteLine($"Got {meshes.Length} meshes");
             for (int i = 0; i < meshes.Length; i++)
             {
                 Console.WriteLine($"  {i + 1} mesh has {meshes[i].Vertices.Count} vrtices");
             }
 
-            Console.WriteLine("press any key to exit");
+            Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
     }
