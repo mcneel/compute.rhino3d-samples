90e3ceb47818bc8bda61cf6f220131b950f15775
kovacsv <viktorkovacs@gmail.com>
2019-02-03 12:55:57 +0100

    Add Nurbs tesselation example.

--- samples/BrepBooleanOperation/Program.cs
+++ samples/BrepBooleanOperation/Program.cs
@@ -27,76 +27,13 @@ namespace BrepBooleanOperation
             allBreps.AddRange(cubeBrep);
             allBreps.AddRange(sphereBrep);
             Brep[] union = BrepCompute.CreateBooleanUnion(allBreps, tolerance);
-            ExportBrepsToObj("cube_sphere_union.obj", union);
+            Rhino.Compute.ObjExport.ExportBrepsToObj("cube_sphere_union.obj", union);
 
             Brep[] intersection = BrepCompute.CreateBooleanIntersection(cubeBrep, sphereBrep, tolerance);
-            ExportBrepsToObj("cube_sphere_intersection.obj", intersection);
+            Rhino.Compute.ObjExport.ExportBrepsToObj("cube_sphere_intersection.obj", intersection);
 
             Brep[] difference = BrepCompute.CreateBooleanDifference(cubeBrep, sphereBrep, tolerance);
-            ExportBrepsToObj("cube_sphere_difference.obj", difference);
-        }
-
-        static void ExportBrepsToObj(string filePath, IEnumerable<Brep> breps)
-        {
-            List<Mesh> meshes = new List<Mesh> ();
-            foreach (Brep brep in breps)
-            {
-                Mesh[] brepMeshes = MeshCompute.CreateFromBrep(brep);
-                foreach (Mesh brepMesh in brepMeshes)
-                {
-                    meshes.Add(brepMesh);
-                }
-            }
-            ExportMeshesToObj(filePath, meshes);
-        }
-
-        static void ExportMeshesToObj (string filePath, IEnumerable<Mesh> meshes)
-        {
-            using (StreamWriter file = new StreamWriter(filePath, false))
-            {
-                int meshIndex = 0;
-                int vertexOffset = 1;
-                foreach (Mesh mesh in meshes)
-                {
-                    if (mesh.Vertices.Count != mesh.Normals.Count)
-                    {
-                        throw new System.Exception ("Different number of vertices and normals in the mesh.");
-                    }
-
-                    file.WriteLine("g Mesh-{0}", meshIndex);
-                    foreach (Point3f vertex in mesh.Vertices)
-                    {
-                        file.WriteLine("v {0} {1} {2}", vertex.X, vertex.Y, vertex.Z);
-                    }
-                    foreach (Vector3f normal in mesh.Normals)
-                    {
-                        file.WriteLine("vn {0} {1} {2}", normal.X, normal.Y, normal.Z);
-                    }
-                    foreach (MeshFace face in mesh.Faces)
-                    {
-                        if (face.IsTriangle)
-                        {
-                            file.WriteLine("f {0}//{1} {2}//{3} {4}//{5}",
-                                vertexOffset + face.A, vertexOffset + face.A,
-                                vertexOffset + face.B, vertexOffset + face.B,
-                                vertexOffset + face.C, vertexOffset + face.C
-                            );
-                        } else if (face.IsQuad)
-                        {
-                            file.WriteLine("f {0}//{1} {2}//{3} {4}//{5} {6}//{7}",
-                                vertexOffset + face.A, vertexOffset + face.A,
-                                vertexOffset + face.B, vertexOffset + face.B,
-                                vertexOffset + face.C, vertexOffset + face.C,
-                                vertexOffset + face.D, vertexOffset + face.D
-                            );
-                        }
-                    }
-
-                    meshIndex += 1;
-                    vertexOffset += mesh.Vertices.Count;
-                }
-
-            }
+            Rhino.Compute.ObjExport.ExportBrepsToObj("cube_sphere_difference.obj", difference);
         }
     }
 }
